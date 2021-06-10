f9d5be57bb8f5e1f9c8942e203df6e2c8d678470
Flavio Santos <flavio.santos@allcomsistemas.com.br>
2021-06-10 10:23:33 -0300

    :bug: Corrigido bug para setar tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle, { lightTheme, darkTheme } from '../styles/global';
@@ -9,6 +9,20 @@ import AppContainer from '../components/AppContainer';
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (typeof localStorage !== 'undefined') {
+      const storageTheme = localStorage.getItem('@GithubExplorer:theme');
+
+      if (storageTheme) {
+        setTheme(JSON.parse(storageTheme));
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:theme', JSON.stringify(theme));
+  }, [theme]);
 
   const toggleTheme = useCallback(() => {
     setTheme(theme === 'light' ? 'dark' : 'light');
